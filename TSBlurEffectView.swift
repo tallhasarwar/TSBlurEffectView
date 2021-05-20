b73221ecaa91678a510fce5b3389d8973a673dd8
Tallha Sarwar <tallhasarwar@gmail.com>
2021-05-20 19:19:20 +0500

    Add files via upload

---
+++ TSBlurEffectView.swift
@@ -0,0 +1,48 @@
+//
+//  TSBlurEffectView.swift
+//  Finja
+//
+//  Created by Tallha Sarwar on 20/05/2021.
+//  Copyright © 2021 Finja Pvt Limited. All rights reserved.
+//
+
+import Foundation
+
+
+class TSBlurEffectView: UIVisualEffectView {
+    
+    var animator = UIViewPropertyAnimator(duration: 1, curve: .linear)
+    
+    var intensity = 1.0
+    
+    override func didMoveToSuperview() {
+        guard let superview = superview else { return }
+        backgroundColor = .clear
+        frame = superview.bounds
+        setupBlur()
+    }
+    
+    private func setupBlur() {
+        animator.stopAnimation(true)
+        effect = nil
+
+        animator.addAnimations { [weak self] in
+            self?.effect = UIBlurEffect(style: .regular)
+        }
+        
+        if intensity > 0 && intensity <= 10 {
+            
+            let value = CGFloat(intensity)/10
+            animator.fractionComplete = value
+            
+        }
+        else {
+            animator.fractionComplete = 0.05
+        }
+        
+    }
+    
+    deinit {
+        animator.stopAnimation(true)
+    }
+}
